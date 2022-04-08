8ea8560456ad37a8c4cf9b9d9e3f5e1fae96dd9d
sabrina <sabrinadjebbar93@gmail.com>
2022-04-08 17:28:48 -0400

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () =>{
+
+   const title = tutorials.map((string) =>{
+ return string.split(" ").map(word => {
+  return word.charAt(0).toUpperCase()+ word.slice(1)
+   }).join(" ")
+
+ });
+   return title 
 }
